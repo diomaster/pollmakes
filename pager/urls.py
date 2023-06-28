0535926261cbb046d970f1a351ad5b8cc0e0f3cc
dallensmith1@mail.weber.edu <Dallen Smith>
2023-06-28 06:08:27 -0600

    trying to fix errors

--- pager/urls.py
+++ pager/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+app_name = 'pager'
+
 urlpatterns = [
     path('', views.index, name = 'index'),
 ]
--- pollmakes/urls.py
+++ pollmakes/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('', include('pager.urls')),
     path('polls/', include('polls.urls')),
